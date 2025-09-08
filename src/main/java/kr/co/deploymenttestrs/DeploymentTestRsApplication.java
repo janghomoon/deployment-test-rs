19ca7105d6c23ba2d7c8645bda6f749f3db1e099
janghomoon <sonyjangho@gmail.com>
2025-09-08 23:46:30 +0900

    Initial commit

---
+++ src/main/java/kr/co/deploymenttestrs/DeploymentTestRsApplication.java
@@ -0,0 +1,13 @@
+package kr.co.deploymenttestrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentTestRsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploymentTestRsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/deploymenttestrs/DeploymentTestRsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.deploymenttestrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentTestRsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
