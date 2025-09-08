cd7580186eaa9be65bcd80caa7c04165a0270115
janghomoon <sonyjangho@gmail.com>
2025-09-08 23:55:51 +0900

    feat -  빌드 오류 수정 및  샘플 호출용 컨틀로러 추가

---
+++ src/main/java/kr/co/deploymenttestrs/controller/DeploymentController.java
@@ -0,0 +1,19 @@
+package kr.co.deploymenttestrs.controller;
+
+import kr.co.deploymenttestrs.service.DeploymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DeploymentController {
+
+    private final DeploymentService deploymentService;
+
+    @GetMapping("/deploy")
+    public ResponseEntity<String> deploy() {
+        return ResponseEntity.ok("Deployment started");
+    }
+}
